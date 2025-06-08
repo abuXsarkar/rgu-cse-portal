9638e4a9857b3a7dff29ac4dd2c937e3fb99bc84
unknown <abu@cyberdude.com>
2025-06-09 01:06:46 +0530

    FIX: Correct import path in index.js for App.tsx

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+import ProductionApp from './App'; // This is the corrected import
 import reportWebVitals from './reportWebVitals';
-import ProductionApp from './App.tsx'; 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ProductionApp />
-  </React.StrictMode>);
+  </React.StrictMode>
+);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
